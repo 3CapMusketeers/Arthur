78b16f7b8710aa36f69d16ba4046ac921bd669c9
Orlando Torres <orlando.torres12@upr.edu>
2020-09-29 19:06:45 -0400

    Changes on Arthur for Spotify Implicit Grant Authorization flow.

--- services/arthur/project/app/handlers/spotify/SpotifyAPI.py
+++ services/arthur/project/app/handlers/spotify/SpotifyAPI.py
@@ -4,11 +4,6 @@ import requests
 import urllib
 
 class SpotifyAPI:
-    """
-    A singleton class for Spotify's API.
-    """
-
-    __instance__ = None
 
     # Urls
     BASE_URL = os.environ.get('BASE_URL')
@@ -22,12 +17,6 @@ class SpotifyAPI:
     CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 
     def __init__(self):
-        if SpotifyAPI.__instance__ is None:
-
-            SpotifyAPI.__instance__ = self
-
-        else:
-            Exception('Cannot create a new SpotifyAPI instance. Use \'get_instance() instead.\'')
 
         self.access_token = None
 
@@ -52,50 +41,6 @@ class SpotifyAPI:
         return '%s?client_id=%s&response_type=code&redirect_uri=%s&scope=user-read-private,user-library-read' % \
               (self.AUTH_URL, self.CLIENT_ID, urllib.parse.quote_plus(self.REDIRECT_URI))
 
-    def request_access_and_refresh_tokens(self, code):
-        """
-        Requests and returns access and refresh tokens.
-        :param code: String
-            From Spofity doc: An authorization code that can be exchanged for an access token.
-        :return: String, String or None, None if something went wrong.
-            The access and refresh tokens.
-        """
-
-        data = {'grant_type': 'authorization_code', 'code': code, 'redirect_uri': self.REDIRECT_URI,
-                'client_id': self.CLIENT_ID, 'client_secret': self.CLIENT_SECRET}
-
-        request = requests.post(self.API_TOKEN_URL, data=data)
-
-        request_json = json.loads(request.text)
-
-        if 'access_token' in request_json and 'refresh_token' in request_json:
-
-            return request_json['access_token'], request_json['refresh_token']
-
-        return None, None
-
-    def request_new_access_token(self, refresh_token):
-        """
-        Returns a new access token. This function should be called when the user's access token has expired.
-        :param refresh_token: String
-            The refresh token that was obtained from the 'request_access_and_refresh_tokens()' function.
-        :return: String or None if something went wrong.
-            The new access token.
-        """
-
-        data = {'grant_type': 'refresh_token', 'refresh_token': refresh_token, 'client_id': self.CLIENT_ID,
-                'client_secret': self.CLIENT_SECRET}
-
-        request = requests.post(self.API_TOKEN_URL, data=data)
-
-        request_json = json.loads(request.text)
-
-        if 'access_token' in request_json:
-
-            return request_json['access_token']
-
-        return None
-
     def set_access_token(self, token):
         """
         Sets a new access token.
@@ -115,14 +60,3 @@ class SpotifyAPI:
     def is_authenticated(self):
 
         return self.access_token and self.refresh_token
-
-    @staticmethod
-    def get_instance():
-
-        if SpotifyAPI.__instance__ is None:
-
-            return SpotifyAPI()
-
-        else:
-
-            return SpotifyAPI.__instance__
