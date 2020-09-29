8d88cad6fdcb27ea644897b9b9bdfa7bd80bce7e
Luis Liz <luis.liz@upr.edu>
2020-09-29 00:18:22 -0400

    Added dev mode to entry

--- services/arthur/entrypoint.sh
+++ services/arthur/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-
 echo "Waiting for postgres..."
-
 while ! nc -z postgres 5432; do
   sleep 0.1
 done
 
 echo "PostgreSQL started"
 
-gunicorn -b 0.0.0.0:5000 manage:app
+FLASK_ENV="development"
+if [ "$FLASK_ENV"=="development" ]; then
+  python manage.py run
+else
+  gunicorn -b 0.0.0.0:5000 manage:app
+fi
