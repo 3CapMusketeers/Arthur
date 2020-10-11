0adf5d77d36d16ea62417920b0be5d6766dddfdb
Orlando Torres <orlando.torres12@upr.edu>
2020-10-11 09:22:25 -0400

    Moved access_token to body.

--- services/arthur/project/app/spotify_blueprint.py
+++ services/arthur/project/app/spotify_blueprint.py
@@ -15,13 +15,14 @@ spotify_blueprint = Blueprint('spotify_blueprint', __name__)
 @spotify_blueprint.route('/', methods=['POST'])
 def index():
     if 'access_token' in request.form:
-        at = request.form['access_token']
-        spotify_api = SpotifyAPI(at)
 
-        # spotify_api.set_access_token(request.args['access_token'])
+        access_token = request.form['access_token']
+
+        spotify_api = SpotifyAPI(access_token)
 
         # Get user profile and insert into db if not already.
         user = spotify_api.get_user_profile()
+
         if 'error' in user:
             return jsonify(error=True, msg=user['error'])
 
@@ -30,11 +31,9 @@ def index():
         # DBHandler().insert_user(user)
 
         return jsonify(user=user['display_name'])
-        # else:
-        #     return jsonify(error=True, msg='Model not created')
+
     else:
-        return jsonify(error=True,msg='No access token. Use spotify URL to authenticate', url=url_for('spotify_blueprint.authorization'))
-    # if 'access_token' in request.args:
+        return jsonify(error=True, msg='No access token. Use spotify URL to authenticate', url=url_for('spotify_blueprint.authorization'))
 
 
 @spotify_blueprint.route('/authorization', methods=['GET'])
@@ -48,28 +47,34 @@ def authorization():
 
     return {'spotify_auth_url': spotify_api.request_authorization_to_access_data_url()}
 
-@spotify_blueprint.route('/users/<access_token>/saved-tracks', methods=['POST']) # Weird name
-def saved_tracks(access_token):
+@spotify_blueprint.route('/users/saved-tracks', methods=['POST']) # Weird name
+def saved_tracks():
 
-    if 'search_term' in request.args:
+    if 'access_token' in request.form and 'search_term' in request.form:
 
         spotify_handler = SpotifyAPIHandler()
 
-        return spotify_handler.saved_tracks(access_token, request.args['search_term'])
+        return spotify_handler.saved_tracks(request.form['search_term'], request.args['search_term'])
 
     return {'Error': 'Search term missing.'}
 
-@spotify_blueprint.route('/users/<access_token>/recommended', methods=['POST'])
-def recommended(access_token):
+@spotify_blueprint.route('/users/recommended', methods=['POST'])
+def recommended():
 
-    if 'search_term' in request.args:
+    if 'access_token' in request.form and 'search_term' in request.form:
 
-        spotify_api = SpotifyAPI(access_token)
         spotify_handler = SpotifyAPIHandler()
-        recommend = spotify_handler.recommended(access_token, request.args['search_term'])
-        if('error' in recommend):
+
+        recommend = spotify_handler.recommended(request.form['search_term'], request.args['search_term'])
+
+        if 'error' in recommend:
+
+            spotify_api = SpotifyAPI(request.form['search_term'])
+
             merlin_api_handler = MerlinAPIHandler(spotify_api)
+
             merlin_api_handler.create_model()
+
             return recommend
     
     return {'Error': 'Search term missing.'}
