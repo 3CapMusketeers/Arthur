ea110acbd088f7c03a59950dea98003a0edd2c25
Orlando Torres <orlando.torres12@upr.edu>
2020-10-31 11:32:28 -0400

    Reorganized MerlinAPI by moving non-request operations to MerlinAPIHandler.

--- services/arthur/project/app/handlers/MerlinAPIHandler.py
+++ services/arthur/project/app/handlers/MerlinAPIHandler.py
@@ -6,17 +6,27 @@ class MerlinAPIHandler:
 
     def __init__(self, spotify_api):
 
-        self.merlin_api = MerlinAPI(spotify_api)
+        self.merlin_api = MerlinAPI()
+
+        self.spotify_api = spotify_api
 
         self.is_user_authenticated = spotify_api.is_authenticated()
 
     def create_model(self):
 
-        if self.is_user_authenticated is True:
-            result = self.merlin_api.create_model()
-            return 'msg' in result and result['msg'] == 'ok'
+        saved_tracks = self.spotify_api.get_user_saved_tracks()
 
-        return self.is_user_authenticated
+        tracks = []
+
+        for track in saved_tracks:
+
+            if 'track' in track and 'preview_url' in track['track'] and track['track']['preview_url'] is not None:
+
+                tracks.append({'id': track['track']['id'], 'url': track['track']['preview_url']})
+
+        result = self.merlin_api.create_model(saved_tracks)
+
+        return 'msg' in result and result['msg'] == 'ok'
 
     def check_model(self, user_id):
 
@@ -28,16 +38,93 @@ class MerlinAPIHandler:
 
     def classify_tracks(self, search_term):
 
-        if self.is_user_authenticated is True:
+        saved_tracks = self.spotify_api.get_user_saved_tracks()
 
-            return jsonify(tracks=self.merlin_api.classify_tracks(search_term))
+        classify_tracks = []
 
-        return is_user_authenticated
+        for track in saved_tracks:
+
+            if 'track' in track and 'preview_url' in track['track'] and track['track']['preview_url'] is not None:
+
+                classify_tracks.append({'id': track['track']['id'], 'url': track['track']['preview_url']})
+
+        playlists = self.spotify_api.search_playlist(search_term, limit=5)
+
+        playlist_tracks = []
+
+        for playlist in playlists:
+
+            playlist_tracks += self.spotify_api.get_tracks_from_playlist(playlist['id'])
+
+        training_tracks = []
+
+        for track in playlist_tracks:
+
+            if 'preview_url' in track and track['preview_url'] is not None:
+
+                training_tracks.append({'id': track['id'], 'url': track['preview_url']})
+
+        response_tracks = self.merlin_api.classify_tracks(search_term)
+
+        if 'msg' in response_tracks:
+
+            return jsonify(tracks={'error': True, 'msg': response_tracks['msg']})
+
+        results = []
+
+        if 'tracks' in response_tracks:
+
+            tracks = self.spotify_api.get_several_tracks(response_tracks['tracks'])
+
+            for track in tracks:
+
+                artists = []
+
+                if 'artists' in track:
+
+                    artist_list = [artist['name'] for artist in track['artists']]
+
+                    artists = ", ".join(map(str, artist_list))
+
+                results.append({'id': track['id'], 'name': track['name'], 'uri': track['uri'], 'artists': artists})
+
+        return jsonify(tracks=results)
 
     def curated_playlist(self, search_term):
 
-        if self.is_user_authenticated is True:
+        playlist_tracks = []
 
-            return jsonify(track=self.merlin_api.curated_playlist(search_term))
+        playlists = self.spotify_api.search_playlist(search_term)
 
-        return is_user_authenticated
+        for playlist in playlists:
+
+            if 'id' in playlist:
+
+                playlist_tracks += self.spotify_api.get_tracks_from_playlist(playlist['id'])
+
+        tracks = []
+
+        for track in playlist_tracks:
+
+            if 'preview_url' in track and track['preview_url'] is not None:
+
+                tracks.append({'id': track['id'], 'url': track['preview_url']})
+
+        response_tracks = self.merlin_api.curated_playlist(search_term, tracks)
+
+        if 'msg' in response_tracks:
+
+            return jsonify(tracks={'error': True, 'msg': response_tracks['msg']})
+
+        results = []
+
+        if 'tracks' in response_tracks:
+
+            tracks = self.spotify_api.get_several_tracks(response_tracks['tracks'])
+
+            for track in tracks:
+
+                results.append({'id': track['id'], 'name': track['name'], 'uri': track['uri'],
+                                'artists': track['artists']})
+
+        return jsonify(tracks=results)
