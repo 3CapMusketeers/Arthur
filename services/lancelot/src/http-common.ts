639bb829f37f742776b716b335c6c3d206cdf681
Luis Liz <luis.liz@upr.edu>
2020-12-07 02:05:39 +0000

    Server ip for http

--- services/lancelot/src/http-common.ts
+++ services/lancelot/src/http-common.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // var path = this.$router.resolve({name: 'Home'}).href
 
 export default axios.create({
-	baseURL: "http://localhost:5000/",
+	baseURL: "http://35.224.48.117:5000/",
   headers: {
     "Content-type": "application/json",
   }
